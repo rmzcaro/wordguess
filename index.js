0c28d1c1618556507d3564c9214102fe8cb8ea15
Carolina RC <caro.rmz315@gmail.com>
2018-04-12 14:47:16 -0500

    created an array of new Letter objects per line 33 in HW

---
+++ index.js
@@ -0,0 +1,3 @@
+// require word.js 
+// select a word and use Word constructor to store it 
+// prompt for each guess and keep tracks of user guess
--- word.js
+++ word.js
@@ -1,21 +1,33 @@
 var Letter = require("./letter.js")
 
- function Word(word){
-    this.new = [];
-    this.word = word; 
-    this.strWord = function(){
-        Letter.call()
+function Word(word, arrayOfWords, letterObjects, wrongletters) {
+    //holds the array of words
+    this.arrayOfWords = ["ski","surf", "dance", "jump"];    
+    // holds the word that is chosen 
+    this.word = word;
+    //number of blanks in array of words
+    this.blank = 0; 
+    //represents the letters of the new  underlying word
+    this.letterObjects = [];
+    //letters guessed wrong
+    this.wrongletters = [];
+    // letters in the word 
+    this.letterStr= function(){
+        word =  arrayOfWords[Math.floor(Math.random() * arrayOfWords.length)];
+// -------- paused here ------- Line 33 in HW Instructions ------------------
+        // return word.split("");
+        //ifGuess goes here
     }
-    // this.char = function(char){
-    // }
+ 
+    // split the word in a letters array
+    // for each letter create a letter object
+    // this is what I need to add to letters object array
+    //for each letter create two objects, one for spaces, one for objects
+
+console.log(word);
+
+
 }
 
+Word();
 
-// example 
-// function Food(name, price) {
-//     Product.call(this, name, price);
-//     this.category = 'food';
-//   }
-
-var letter1 = new Letter("a");
-letter1.ifGuess();
